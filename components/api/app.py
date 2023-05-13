e68c52542081e293c4e3177104bb2959d89b0b92
waheb-1988 <hocineabdelouaheb@yahoo.fr>
2023-05-13 15:39:05 +0100

    update

--- components/api/app.py
+++ components/api/app.py
@@ -5,18 +5,24 @@ from flask_cors import CORS, cross_origin
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), "../../"))
 sys.path.append(parent_dir)
 from flask import Flask, request, jsonify
-from components.models.preprocessing import predictions_1
+from components.models.preprocessing_list import predictions_1
 import pickle 
 import pathlib
 import os  
 import pandas as pd
+import random
+
+def generate_random_integer(start, end):
+    return random.randint(start, end)
+
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 dir_path = pathlib.Path(__file__).parent.parent
 output = dir_path / "output"
 path = dir_path / "data" 
-file_name = "data.csv"
+dff= pd.read_csv(os.path.join(path, "data_lean.csv"))
 
 with open("model.pkl", "rb") as file:
    model = pickle.load(file)
@@ -27,12 +33,9 @@ with open("model.pkl", "rb") as file:
 
 def predict():
     data = request.json
-    print(data)
-    dd = pd.DataFrame(data)
-    print("adddd")
-    print(dd)
-    data_in= predictions_1.(dd)
-    predictions = model.predict(data_in)
+    random_number = random.randint(1, 1000) 
+    dd=dff.iloc[[random_number]]
+    predictions = model.predict(dd)
 
     return str(np.round(predictions,2))
 
